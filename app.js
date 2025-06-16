58da49687510b17ed82841ad09a8a8c6d0320ae1
Greg Merrill <gmerrill03@gmail.com>
2025-06-16 12:48:03 -0700

    add some more to app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-var express = require("express");
-var app = express();
-const records = require("./records.js");
+var express = require("express")
+var app = express()
+const records = require("./records.js")
 
-app.get("/", (req, res) => {
-  res.send("Welcome to movies API");
-});
+app.use(express.json())
+app.set("json spaces", 2)
+
+app.post("/movies", async (req, res) => {
+  const newMovie = req.body
+  const addedMovie = records.createMovie(newMovie)
+	res.json(addedMovie)
+})
+
+app.get("/movies", async (req, res) => {
+	const movies = await records.getMoviesList()
+	res.json(movies)
+})
+
+app.get("/movies/:id", async (req, res) => {
+	const movie = await records.getMovieById(req.params.id)
+	res.json(movie)
+})
+
+app.get("/", async (req, res) => {
+	res.send("Welcome to the movies API!")
+})
 
 app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+	console.log("Server is listening on port 3000")
+})
